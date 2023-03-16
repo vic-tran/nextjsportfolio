8b55f9b71d6d00861d60e1766ce53babf4cf6598
Victor Tran <vc.tran998@gmail.com>
2023-03-15 19:46:33 -0700

    About section change

--- components/About.jsx
+++ components/About.jsx
@@ -11,9 +11,8 @@ const About = () => {
                 <h2 className='pt-10'>Bio</h2>
                 <Image className='scale-75 rounded-full shadow-2xl' src={me} alt=""></Image>
                 <p className='pb-20 text-gray-600 max-w-[60%] m-auto'>I'm a self taught web developer with a passion for creating design forward 
-                and responsive applications. The process of designing feautures from my ideas as well as outside inspiration into production 
-                is something that intrigues me the most about development. I'm a firm believer in 
-                the fact that learning never stops and that there is always a new skill to pick up. In my free time I love to surf, read, and hangout with friends!
+                and responsive applications. The process of creating things from my ideas and inspirations 
+                is what intrigues me the most about development.  In my free time I love to surf, read, and hangout with friends!
                 </p>
               
                
